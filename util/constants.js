d2d719d55380e3bb8038b3b2d61917cddb6f2af5
srdjankokot <kokotsrdjan@gmail.com>
2024-08-02 09:58:54 +0200

    change port

--- util/constants.js
+++ util/constants.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5001;
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 5001;
 
 const ee = require('@google/earthengine');
 
